d77459ac1a2fadba445219b2cb2e3b2716a04f40
SloththeSid <143199212+SloththeSid@users.noreply.github.com>
2024-09-19 10:03:24 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,23 +2,24 @@
 # SPDX-License-Identifier: MPL-2.0
 
 variable "project_id" {
-  description = "project id"
+  description = "Project ID"
+  default     = "teratask-436014"  # Add your project ID as default
 }
 
 variable "region" {
-  description = "region"
+  description = "Region"
+  default     = "asia-south1"  # Add your region as default
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
-
 
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
+  auto_create_subnetworks  = false  # No need for quotes around false
 }
 
 # Subnet
@@ -27,4 +28,15 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
+}
+
+# Optional Outputs (if needed)
+output "vpc_name" {
+  description = "The name of the VPC"
+  value       = google_compute_network.vpc.name
+}
+
+output "subnet_name" {
+  description = "The name of the Subnet"
+  value       = google_compute_subnetwork.subnet.name
 }
