facf660563d165d1f07d2a8f967c2374c0b8ba7c
SloththeSid <143199212+SloththeSid@users.noreply.github.com>
2024-09-19 10:08:06 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -12,9 +12,11 @@ variable "region" {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  credentials = file("${path.module}/credentials.json")
+  project     = var.project_id
+  region      = var.region
 }
+
 
 # VPC
 resource "google_compute_network" "vpc" {
